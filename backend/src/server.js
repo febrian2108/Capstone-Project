33167384409498d5774baae2c9bca63ab9bf8a9b
Faiz <faizbot81@gmail.com>
2025-06-01 16:19:35 +0700

    Menambahkan folder backend

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const authRoutes = require('./routes/authRoutes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(authRoutes);
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
