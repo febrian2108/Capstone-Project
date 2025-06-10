a42f4b5a0315f2c1ae95db58f1443789192e3c97
Faiz <faizbot81@gmail.com>
2025-06-10 17:39:32 +0700

    Update server.js dari backend-feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,14 @@
 
 const Hapi = require('@hapi/hapi');
 const authRoutes = require('./routes/authRoutes');
+const genreRoutes = require('./routes/genre');
+const filmRoutes = require('./routes/film');
+const recomendationRoutes = require('./routes/recommendation');
+const optionsRoutes = require('./routes/optionsRoutes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: 9000,
         host: 'localhost',
         routes: {
             cors: {
@@ -14,7 +18,7 @@ const init = async () => {
         },
     });
 
-    server.route(authRoutes);
+    server.route([...authRoutes, ...genreRoutes, ...filmRoutes, ...recomendationRoutes, ...optionsRoutes]);
 
     await server.start();
     console.log(`Server running on ${server.info.uri}`);
